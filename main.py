bc8fa6aad30d52635da562a1fd6cc11160129be2
Shamaal <notshamaald@gmail.com>
2024-04-07 22:02:01 +0100

    commit 7: - updated display_booking_options() - updated make_a_booking()

--- main.py
+++ main.py
@@ -10,41 +10,57 @@ def get_user_choice() -> int:
     """
     print(f"\n{'='*48}\n")
     print("What would you like to do?")
-    print("-"*26)
+    print("-"*27)
     for index, option in enumerate(FUNCTION_OPTIONS, 1):
-        print(f"[{index}] {option}")
-    print("-" * 26)
+        print(f"[{index}] {option:<22}|")
+    print("-" * 27)
     user_choice = my_functions.get_user_int_in_range(1, 5, "Enter index number to select choice: ")
     print(f"\n{'='*48}\n")
     return user_choice
 
 
-def display_booking_options() -> list:
-    availability = []
-
-    print(FUNCTION_OPTIONS[0])
-    print("-"*48)
-    print(f"{'ID':<3}|{'Equipment':^15}|{'Daily rent':^12}|{'Note':^13}|")
-    print("-" * 48)
+def display_booking_options() -> tuple[list, list]:
+    """
+    displays available booking options
+    :return: equip, rent
+    """
+    print("-"*34)
+    print(f"{'ID':<3}|{'Equipment':^15}|{'Daily rent':^12}|")
+    print("-" * 34)
     with open("equipment_data.txt", "r") as file:
         index = 0
+        equip = []
+        rent = []
         for raw_line in file:
-            index += 1
             line = raw_line.rstrip()
             line = line.split(",")
             if int(line[2]) > 0:
-                print(f"[{index}]|{line[0]:<15}| ${float(line[1]):>10,.2f}|{'':>13}|")
-                availability.append(True)
-            else:
-                print(f"[{index}]|{line[0]:<15}| ${float(line[1]):>10,.2f}|{'unavailable':^13}|")
-                availability.append(False)
-    print("-" * 48)
+                equip.append(line[0])
+                rent.append(float(line[1]))
+                print(f"[{index+1}]  {equip[index]:<14}| ${rent[index]:>10,.2f}|")
+                index += 1
 
-    return availability
+    print("-" * 34)
+
+    return equip, rent
 
 
 def make_a_booking():
-    availability = display_booking_options()
+    print(FUNCTION_OPTIONS[0])
+
+    first_name = my_functions.get_valid_name(True)
+    last_name = my_functions.get_valid_name(False)
+    phone_number = my_functions.get_valid_phone()
+
+    equipment, rent = display_booking_options()
+
+    chosen_equip = my_functions.get_user_int_in_range(1, len(equipment)+1, "Enter index number to select equipment: ")
+    rent_duration = my_functions.get_user_int_in_range(1, 8, f"How many days would you to rent \"{equipment[chosen_equip-1]}\" for?: ")
+
+    total_cost = rent[chosen_equip] * rent_duration
+
+    with open(f"{first_name}_{last_name}.txt", "w") as file:
+        print(f"{first_name},{last_name},{phone_number},{equipment},{rent_duration},{total_cost}", file=file)
 
 
 
