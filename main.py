5f60e99fe517f1b0cd25ddac5dff8883485cd9eb
Shamaal <notshamaald@gmail.com>
2024-04-07 20:41:49 +0100

    commit 6: - added display_booking_options() to my_functions.py - added bookings_2024.txt

--- main.py
+++ main.py
@@ -19,8 +19,33 @@ def get_user_choice() -> int:
     return user_choice
 
 
+def display_booking_options() -> list:
+    availability = []
+
+    print(FUNCTION_OPTIONS[0])
+    print("-"*48)
+    print(f"{'ID':<3}|{'Equipment':^15}|{'Daily rent':^12}|{'Note':^13}|")
+    print("-" * 48)
+    with open("equipment_data.txt", "r") as file:
+        index = 0
+        for raw_line in file:
+            index += 1
+            line = raw_line.rstrip()
+            line = line.split(",")
+            if int(line[2]) > 0:
+                print(f"[{index}]|{line[0]:<15}| ${float(line[1]):>10,.2f}|{'':>13}|")
+                availability.append(True)
+            else:
+                print(f"[{index}]|{line[0]:<15}| ${float(line[1]):>10,.2f}|{'unavailable':^13}|")
+                availability.append(False)
+    print("-" * 48)
+
+    return availability
+
+
 def make_a_booking():
-    print("hewwo")
+    availability = display_booking_options()
+
 
 
 def main():
