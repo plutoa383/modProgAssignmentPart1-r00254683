5f62a770f19832d39db6c110df5baeacc4905608
Shamaal <notshamaald@gmail.com>
2024-04-07 18:15:35 +0100

    commit 3: - added get_valid_name() to my_functions.py -added get_valid_phone() to my_functions.py

--- my_modules/my_functions.py
+++ my_modules/my_functions.py
@@ -1,7 +1,7 @@
 def get_pos_num(prompt: str) -> int:
     """
-    gets and validates a positive integer from user
-    :param prompt: string used as prompt to ask user
+    gets a positive integer from user
+    :param prompt: string used to interact with the user
     :return: valid_num
     """
     while True:
@@ -17,6 +17,65 @@ def get_pos_num(prompt: str) -> int:
     return valid_num
 
 
+def get_non_empty_string(prompt: str) -> str:
+    """
+    gets a non-empty string from the user
+    :param prompt: string used to interact with the user
+    :return: valid_str
+    """
+    while True:
+        valid_str = input(prompt)
+        if len(valid_str) > 0:
+            break
+        else:
+            print("enter a valid input")
+
+    return valid_str
+
+
+def get_valid_name(first_or_last: bool) -> str:
+    """
+    gets valid first or last name from the user
+    :param first_or_last: boolean used to ask for either first (True) or last (False) name
+    :return: valid_name
+    """
+    while True:
+        if first_or_last:
+            valid_name = get_non_empty_string("Enter your first name: ")
+        else:
+            valid_name = get_non_empty_string("Enter your last name: ")
+
+        if len(valid_name) < 20 and valid_name.isalpha():
+            break
+        else:
+            print("enter a valid input")
+
+    return valid_name
+
+
+def get_valid_phone() -> int:
+    while True:
+        valid_phone = get_pos_num("Enter your phone number: ")
+        if len(f"{valid_phone}") == 10:
+            break
+        else:
+            print("enter a valid 10 digit phone number")
+
+    return valid_phone
+
+
 if __name__ == '__main__':
     # get_pos_num() test
-    get_pos_num("enter positive integer")
+    print(get_pos_num("Enter positive integer"))
+
+    # get_non_empty_string() test
+    print(get_non_empty_string("Enter a string"))
+
+    # get_valid_name() test
+    print(get_valid_name(True))
+    print(get_valid_name(False))
+
+    # get_valid_phone() test
+    print(get_valid_phone())
+
+
